6c9ccc145cfff009b629f983c75609e022192e02
suryabhan pandey <suryabhan.pandey@getfareye.com>
2022-10-17 22:45:52 +0530

    day1

--- day1/starter/index.js
+++ day1/starter/index.js
@@ -3,38 +3,40 @@
 //3. configurable field;
 
 const nameFormatter = (user) => `${user.firstName} ${user.lastName}`;
-
-const uservariable = [
-        {
-            label: "Name",
-            value: "firstName",
-            //formatter : nameFormatter,
-        },
-        {
-            label: "Age",
-            value: "age",
-        },
-        {
-            label: "Phone no.",
-            value: "phone",
-        },
-        {
-            label: "Blood Group",
-            value: "bloodGroup",
-        },
-        {
-            label: "Company",
-            value: "name",
-          },
-          {
-            label: "Position",
-            value: "name",
-          },
-          {
-            label: "weight ",
-            value: "weight",
-          },
-
+const companyPositionFormatter = (user) => `${user.company.title}`;
+const companyNameFormatter = (user) => `${user.company.name}`;
+let uservariable = [
+  {
+    label: "Name ",
+    value: "name",
+    formatter: nameFormatter,
+  },
+  {
+    label: "Age",
+    value: "age",
+  },
+  {
+    label: "Phone ",
+    value: "phone",
+  },
+  {
+    label: "Blood Group",
+    value: "bloodGroup",
+  },
+  {
+    label: "Company",
+    value: "name",
+    formatter: companyNameFormatter,
+  },
+  {
+    label: "Position",
+    value: "name",
+    formatter: companyPositionFormatter,
+  },
+  {
+    label: "weight ",
+    value: "weight",
+  },
 ];
 
 
@@ -88,102 +90,27 @@ function fromStarting(data){
    let div = document.createElement("div");
    div.setAttribute("class", "items");
 
-
-    //console.log(ele)
     
      uservariable.forEach((ele1) => {
-    let { label, value } = ele1;
-    // if (formatter) {
-    //   value = formatter(users);
-    // } else {
-    //   //value = users[value];
+    let { label,formatter, value } = ele1;
+    if (formatter) {
+      value = formatter(ele);
+    } else {
+      value = ele[value];
       
-    // }
+    }
 
-   // console.log(ele1)
-   let name1="firstName"
-    console.log(ele.name1);
    
     div.appendChild(setNameValue(label, value));
   });
     
 
 
-// //1st div;
-//     let div1 = document.createElement("div");
-//     div1.setAttribute("class", "item");
-
-//     let h31 = document.createElement("h3");
-//     h31.setAttribute("class", "itemdefine");
-//     h31.innerText = "Name";
-
-//     let span1 = document.createElement("span");
-//     span1.setAttribute("class", "itemdefinespan");
-//     span1.innerText = ele.firstName +" "+ ele.lastName;
-
-//     //2nd div;
-//     let div2 = document.createElement("div");
-//     div2.setAttribute("class", "item");
-
-//     let h32 = document.createElement("h3");
-//     h32.setAttribute("class", "itemdefine");
-//     h32.innerText = "Age";
-
-//     let span2 = document.createElement("span");
-//     span2.setAttribute("class", "itemdefinespan");
-//     span2.innerText = ele.age;
-
-//     //3rd div;
-//     let div3 = document.createElement("div");
-//     div3.setAttribute("class", "item");
-
-//     let h33 = document.createElement("h3");
-//     h33.setAttribute("class", "itemdefine");
-//     h33.innerText = "Phone no.";
-
-//     let span3 = document.createElement("span");
-//     span3.setAttribute("class", "itemdefinespan");
-//     span3.innerText = ele.phone; 
-
-//     //4th div;
-//     let div4 = document.createElement("div");
-//     div4.setAttribute("class", "item");
-
-//     let h34 = document.createElement("h3");
-//     h34.setAttribute("class", "itemdefine");
-//     h34.innerText = "Blood Group";
-
-//     let span4 = document.createElement("span");
-//     span4.setAttribute("class", "itemdefinespan");
-//     span4.innerText = ele.bloodGroup;
-
-   
-
-
     mainBox.appendChild(mainboxchild);
 
       mainboxchild.appendChild(image);
       mainboxchild.appendChild(div);
-     
-      
-
-    //  div.appendChild(div1);
-    //  div1.appendChild(h31);
-    //  div1.appendChild(span1);
-
-
-    //  div.appendChild(div2);
-    //  div2.appendChild(h32);
-    //  div2.appendChild(span2);
-
-    //  div.appendChild(div3);
-    //  div3.appendChild(h33);
-    //  div3.appendChild(span3);
-
-    //  div.appendChild(div4);
-    //  div4.appendChild(h34);
-    //  div4.appendChild(span4);
-
+   
     }) ;
 
 }
